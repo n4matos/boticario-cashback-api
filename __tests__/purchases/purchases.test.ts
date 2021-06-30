f44902d0c09b4d28e6abbb0c92e50945ab2d0ba3
Nathan Angelo <nathannangelo@gmail.com>
2021-06-30 14:45:42 -0300

    added test for api external

--- __tests__/purchases/purchases.test.ts
+++ __tests__/purchases/purchases.test.ts
@@ -144,8 +144,15 @@ describe('auth endpoint', function () {
     });
 
 
-
-
+    it('should allow a GET to a external API to check cashback', async function () {
+      const res = await request
+        .get(`/cashback`)
+        .send();
+      expect(res.status).to.equal(200);
+      expect(res.body).not.to.be.empty;
+      expect(res.body).to.be.an('object');
+      expect(res.body.credit).to.be.a('number');
+    });
 
   });
 });
