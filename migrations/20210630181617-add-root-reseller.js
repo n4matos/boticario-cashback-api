266f7deaa506bf5cdc1fdb032b770657a8c0a510
Nathan Angelo <nathannangelo@gmail.com>
2021-06-30 17:36:03 -0300

    added migration for mongodb

---
+++ migrations/20210630181617-add-root-reseller.js
@@ -0,0 +1,14 @@
+module.exports = {
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    await db.collection('users').insertOne({ name: 'Root Reseller', cpf: '153.509.460-56' });
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  }
+};
