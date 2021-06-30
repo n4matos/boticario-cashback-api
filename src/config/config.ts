6ba89b6bccff84d8fbcccd51c9ec88791ebefb27
Nathan Angelo <nathannangelo@gmail.com>
2021-06-30 17:35:41 -0300

    change variable name for root reseller cpf

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || "superencryptedsecret";
-const ROOT_RESELLER = process.env.ROOT_RESELLER;
+const ROOT_RESELLER_CPF = process.env.ROOT_RESELLER_CPF;
 const API_ACCUMULATE_CASHBACK = process.env.API_ACCUMULATE_CASHBACK;
 
 
 const config = {
   jwtSecret: JWT_SECRET,
-  rootReseller: ROOT_RESELLER,
+  rootResellerCpf: ROOT_RESELLER_CPF,
   apiAccumulateCashback: API_ACCUMULATE_CASHBACK
 }
 
