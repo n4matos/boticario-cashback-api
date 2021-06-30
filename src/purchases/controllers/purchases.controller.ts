cbfbb09a497dab7447c275858a5ad1d4b19fe436
Nathan Angelo <nathannangelo@gmail.com>
2021-06-30 13:14:57 -0300

    removed  try catch from purchase controller

--- src/purchases/controllers/purchases.controller.ts
+++ src/purchases/controllers/purchases.controller.ts
@@ -12,93 +12,44 @@ const log: debug.IDebugger = debug('app:purchases-controller');
 class PurchasesController {
 
   async listPurchases(req: express.Request, res: express.Response) {
-    try {
-      const users = await purchasesService.list(100, 0);
-      res.status(200).send(users);
-    } catch (error) {
-      res.status(400).json({
-        message: error.message,
-        error,
-      })
-    }
+    const users = await purchasesService.list(100, 0);
+    res.status(200).send(users);
   }
 
   async getPurchaseById(req: express.Request, res: express.Response) {
-    try {
-      const user = await purchasesService.readById(req.body.id);
-      if (user) {
-        res.status(200).send(user);
-      } else {
-        res.status(200).json({
-          message: 'Purchase has not found'
-        })
-      }
-    } catch (error) {
-      res.status(400).json({
-        message: error.message,
-        error,
+    const user = await purchasesService.readById(req.body.id);
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res.status(200).json({
+        message: 'Purchase has not found'
       })
     }
   }
 
   async getCashbackExternalApi(req: express.Request, res: express.Response) {
-    try {
-      const cashback = await purchasesService.getCashbackExternalApi();
-      res.status(200).send(cashback);
-    } catch (error) {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    }
+    const cashback = await purchasesService.getCashbackExternalApi();
+    res.status(200).send(cashback);
   }
 
   async createPurchase(req: express.Request, res: express.Response) {
-    try {
-      const purchaseId = await purchasesService.create(req.body);
-      res.status(201).send({ id: purchaseId });
-    } catch (error) {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    }
+    const purchaseId = await purchasesService.create(req.body);
+    res.status(201).send({ id: purchaseId });
   }
 
   async put(req: express.Request, res: express.Response) {
-    try {
-      log(await purchasesService.putById(req.body.id, req.body));
-      res.status(204).send();
-    } catch (error) {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    }
+    log(await purchasesService.putById(req.body.id, req.body));
+    res.status(204).send();
   }
 
   async patch(req: express.Request, res: express.Response) {
-    try {
-      log(await purchasesService.patchById(req.body.id, req.body));
-      res.status(204).send();
-    } catch (error) {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    }
+    log(await purchasesService.patchById(req.body.id, req.body));
+    res.status(204).send();
   }
 
   async removePurchase(req: express.Request, res: express.Response) {
-    try {
-      log(await purchasesService.deleteById(req.body.id));
-      res.status(204).send();
-    } catch (error) {
-      return res.status(500).json({
-        message: error.message,
-        error,
-      });
-    }
+    log(await purchasesService.deleteById(req.body.id));
+    res.status(204).send();
   }
 }
 
