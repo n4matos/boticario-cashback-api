71c3e477ad516b5deb3aa5260ff3c928394be2c9
Nathan Angelo <nathannangelo@gmail.com>
2021-06-30 10:44:25 -0300

    removing unnecessary comments and export server for tests

--- app.ts
+++ app.ts
@@ -22,13 +22,10 @@ const port = 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
-
 app.use(express.json());
 app.use(cors());
 app.use(errorHandler);
 
-// here we are preparing the expressWinston logging middleware configuration,
-// which will automatically log all HTTP requests handled by Express.js
 const loggerOptions: expressWinston.LoggerOptions = {
   transports: [new winston.transports.Console()],
   format: winston.format.combine(
@@ -39,31 +36,27 @@ const loggerOptions: expressWinston.LoggerOptions = {
 };
 
 if (!process.env.DEBUG) {
-  loggerOptions.meta = false; // when not debugging, log requests as one-liners
+  loggerOptions.meta = false;
+  if (typeof global.it === 'function') {
+    loggerOptions.level = 'http';
+  }
 }
 
-// initialize the logger with the above configuration
 app.use(expressWinston.logger(loggerOptions));
 
 routes.push(new AuthRoutes(app));
-
-// here we are adding the UserRoutes to our array,
-// after sending the Express.js application object to have the routes added to our app!
 routes.push(new UsersRoutes(app));
 routes.push(new PurchasesRoutes(app));
 
-// this is a simple route to make sure everything is working properly
 const runningMessage = `Server running at http://localhost:${port}`;
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).send(runningMessage)
 });
 
-server.listen(port, () => {
+export default server.listen(port, () => {
   routes.forEach((route: CommonRoutesConfig) => {
     debugLog(`Routes configured for ${route.getName()}`);
   });
-  // our only exception to avoiding console.log(), because we
-  // always want to know when the server is done starting up
   console.log(runningMessage);
 });
